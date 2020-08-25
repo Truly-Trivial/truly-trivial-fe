3673f2bb358ae33a041d882749bd9fa476fb54c8
Thomas Stussi <tstussi06@gmail.com>
2020-08-24 17:05:11 -0700

    file structure and pathing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch,
+  Link
+} from 'react-router-dom';
+import HomePage from './HomePage/HomePage.js';
+import QuizPage from './QuizPage/QuizPage.js';
+import FavoritesPage from './FavoritesPage/FavoritesPage.js';
+import DetailPage from './DetailPage/DetailPage.js';
+import AboutUsPage from './AboutUsPage/AboutUsPage.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+      return (
+          <div>
+              <Router>
+                <main>
+                  <div>
+                    <Link to='/'>HOME PAGE</Link>
+                    <Link to='/quiz'>QUIZ PAGE</Link>
+                    <Link to='/favorites'>FAVORITES PAGE</Link>
+                    <Link to='/about'>ABOUT US PAGE</Link>
+                  </div>
+                  <Switch>
+                      <Route 
+                          path="/" 
+                          exact
+                          render={(routerProps) => <HomePage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/quiz" 
+                          exact
+                          render={(routerProps) => <QuizPage {...routerProps} />} 
+                      />
+                      <Route 
+                        path="/favorites" 
+                        exact
+                        render={(routerProps) => <FavoritesPage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/detail" 
+                          exact
+                          render={(routerProps) => <DetailPage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/about" 
+                          exact
+                          render={(routerProps) => <AboutUsPage {...routerProps} />} 
+                      />
+                  </Switch>
+                </main>
+              </Router>
+          </div>
+      )
+  }
 }
-
-export default App;
