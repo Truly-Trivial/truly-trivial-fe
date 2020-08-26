e6a627405315efada2d9cd514e8adab02e264d63
Patrick Wilson <pmwilson333@gmail.com>
2020-08-25 17:42:13 -0700

    about page

--- src/AboutUsPage/AboutUsPage.js
+++ src/AboutUsPage/AboutUsPage.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './AboutUsPage.css';
 
 export default class AboutUsPage extends Component {
+
     render() {
         return (
-            <div>
-                *****ABOUT US PAGE*****
+            <div className='about-us-page'>
+                <h1 className='about-us-title glow-yellow'>About Us</h1>
+                <div className='about-area glitter-orange'>
+                    <div className='about-area2'>
+                        <h3>Name</h3>
+                        <p>Here are some words describing name. We will probably write a few sentences and they should be a little longer than this, but not too long or else it will look very busy for mobile users.</p>
+                        <p>We will put a link to our linkedin page</p>
+                    </div>
+                </div>
+                <div className='about-area glitter-orange'>
+                    <div className='about-area2'>
+                        <h3>Name</h3>
+                        <p>Here are some words describing name. We will probably write a few sentences and they should be a little longer than this, but not too long or else it will look very busy for mobile users.</p>
+                        <p>We will put a link to our linkedin page</p>
+                    </div>
+                </div>
+                <div className='about-area glitter-orange'>
+                    <div className='about-area2'>
+                        <h3>Name</h3>
+                        <p>Here are some words describing name. We will probably write a few sentences and they should be a little longer than this, but not too long or else it will look very busy for mobile users.</p>
+                        <p>We will put a link to our linkedin page</p>
+                    </div>
+                </div>
+                <div className='about-area glitter-orange'>
+                    <div className='about-area2'>
+                        <h3>Name</h3>
+                        <p>Here are some words describing name. We will probably write a few sentences and they should be a little longer than this, but not too long or else it will look very busy for mobile users.</p>
+                        <p>We will put a link to our linkedin page</p>
+                    </div>
+                </div>
             </div>
         )
     }
