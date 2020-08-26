2d3a4e30c2ecdf06ce8aacbe014d360a8e11bf78
SarahRector <rector.sarah.r@gmail.com>
2020-08-25 17:39:33 -0700

    added spinning bob barker

--- src/FavoritesPage/FavoritesPage.js
+++ src/FavoritesPage/FavoritesPage.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import bob from './bob_barker_sticker_face_2.png';
+import './FavoritesPage.css';
 
 export default class FavoritesPage extends Component {
     render() {
         return (
+            <>
             <div>
                 *****FAVORITES PAGE*****
             </div>
+            <div>
+            <img className="barker" src={bob} alt="bob barker and his smiling face" />
+        </div>
+        </>
         )
     }
 }
